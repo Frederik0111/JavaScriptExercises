519731661c2c65d1a037da622300372121e748a2
Frederik0111 <fredez0111@gmail.com>
2023-02-20 15:52:09 +0100

    second

--- ex4-script.js
+++ ex4-script.js
@@ -1,7 +1,16 @@
 window.addEventListener("load", start);
 
 function start() {
-    console.log("start");
-    
+  console.log("start");
+  document
+    .querySelector("#orange_container")
+    .addEventListener("click", clickOrange);
 }
 
+function clickOrange() {
+    console.log("jump-once");
+    document.querySelector("#orange_sprite")
+        .classList.add("jump-once");
+}
+
+// mangler løsning til en reset
